8673efabbcd256a1584d156e0f6ad79f99f84d77
ramzandashkar <dashkarov@gmail.com>
2023-06-26 12:59:41 +0300

    Add models

---
+++ myshop/shop/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    pass
