82cc6366f51e621478b9e2b112483702eba86707
ramzandashkar <dashkarov@gmail.com>
2023-06-30 13:58:22 +0300

    Add rabbitmq and celery

---
+++ myshop/orders/tasks.py
@@ -0,0 +1,22 @@
+from celery import shared_task
+from django.core.mail import send_mail
+
+from .models import Order
+
+
+@shared_task
+def order_created(order_id):
+    """
+    Задание по отправке уведомления по email
+    при успешном создании заказа.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = f'Order nr. {order.id}'
+    message = f'Dear {order.first_name}, \n\n You have successfully placed an order. Your order ID is {order.id}.'
+    mail_sent = send_mail(
+        subject,
+        message,
+        'admin@myshop.com',
+        [order.email],
+    )
+    return mail_sent
