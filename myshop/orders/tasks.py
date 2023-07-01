d04fc66b63ba0c77b6d55e410cad06bbe472be05
ramzandashkar <dashkarov@gmail.com>
2023-07-01 10:20:23 +0300

    Install Celery and Flower

--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -1,22 +1,21 @@
 from celery import shared_task
 from django.core.mail import send_mail
-
 from .models import Order
 
 
 @shared_task
 def order_created(order_id):
     """
-    Задание по отправке уведомления по email
-    при успешном создании заказа.
+    Task to send an e-mail notification when an order is
+    successfully created.
     """
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
-    message = f'Dear {order.first_name}, \n\n You have successfully placed an order. Your order ID is {order.id}.'
-    mail_sent = send_mail(
-        subject,
-        message,
-        'admin@myshop.com',
-        [order.email],
-    )
+    message = f'Dear {order.first_name},\n\n' \
+              f'You have successfully placed an order.' \
+              f'Your order ID is {order.id}.'
+    mail_sent = send_mail(subject,
+                          message,
+                          'admin@myshop.com',
+                          [order.email])
     return mail_sent
